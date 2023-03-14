89e33cd90a667ff32f25ad5eac48501652c05a0f
danhpch2802 <56262591+huudanh2802@users.noreply.github.com>
2023-03-15 00:52:35 +0700

    Merge branch 'develop' into PS-49

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,11 @@
 import * as express from "express";
 import { asyncRouteHandler } from "../common/middlewares/async-route.handler.js";
-import { getLoginUri, getUserInfomationAsync, obtainLoginTokenAsync,getSignupUri } from "../controllers/auth.controller.js";
+import {
+    checkUserLoginState,
+    getLoginUri,
+    getUserInfomationAsync,
+    obtainLoginTokenAsync,getSignupUri,
+} from "../controllers/auth.controller.js";
 
 export const router = express.Router();
 
@@ -9,5 +14,7 @@ router.get("/login_url", asyncRouteHandler(getLoginUri));
 router.post("/token", asyncRouteHandler(obtainLoginTokenAsync));
 
 router.get("/userinfo", asyncRouteHandler(getUserInfomationAsync));
+
+router.get("/state", asyncRouteHandler(checkUserLoginState));
 
 router.get("/signup",asyncRouteHandler(getSignupUri));
