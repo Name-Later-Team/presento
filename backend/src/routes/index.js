9dfaf2c324f542839af51247ac3dec97be04050d
Lê Minh Huy <60875712+huyleminh@users.noreply.github.com>
2023-03-17 10:08:42 +0700

    Update index.js
    Fix handle error in routing

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,26 +2,27 @@ import axios from "axios";
 import * as express from "express";
 import { asyncRouteHandler } from "../common/middlewares/async-route.handler.js";
 import { ResponseBuilder } from "../common/utils/builders/response.builder.js";
+import { Logger } from "../common/utils/logger.js";
 import { APP_CONFIG } from "../configs/index.js";
 
 export const router = express.Router();
 
 router.get(
-    "/:service/v1/*",
+    "/:service/:path",
     asyncRouteHandler((req, res) => {
-        const { accessToken, tokenType } = req.session?.user ?? {};
-        const headers = { Authorization: `${tokenType} ${accessToken}` };
-
         axios({
             method: req.method,
-            url: `${APP_CONFIG.apiGateway}${req.originalUrl.replace('/api', '')}`,
-            headers,
+            url: `${APP_CONFIG.apiGateway}/${req.params.service}/${req.params.path}`,
         })
             .then((response) => res.send(response.data))
             .catch((error) => {
-                if (axios.isAxiosError(error)) {
+                if (error.response) {
                     return res.status(error.response.status).send(error.response.data);
                 }
+
+                Logger.error(error);
+
+                // todo: handle error here
 
                 return res.status(504).json({
                     code: 504,
