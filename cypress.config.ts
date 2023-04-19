6594ac909d6f8cc08ef0f6ce66b2d066fee096a9
danhpch2802 <56262591+huudanh2802@users.noreply.github.com>
2023-04-19 15:18:48 +0700

    PS-88

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,22 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3001',
+    viewportHeight:900,
+    viewportWidth:1600,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium') {
+          const browserWidth = 1980;
+          const browserHeight = 1080;
+
+          launchOptions.args.push('--start-fullscreen')
+          launchOptions.args.push(`--window-size=${browserWidth},${browserHeight}`)
+        }
+        return launchOptions
+
+      })
     },
-    experimentalStudio: true
+    experimentalStudio: true,
+
   },
 });
